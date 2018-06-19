98ed4c668293fb4d6c1fa438a5d9748b006b7760
Dawid Dutkiewicz <dawid.dutkiewicz@gmail.com>
2018-06-19 16:09:41 +0200

    - initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import json
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+LOG_FILE = os.path.join(os.path.dirname(os.path.abspath(__name__)), 'logfile.log')
+
+
+@app.route('/', methods = ['GET'])
+def main():
+    if request.method == 'GET':
+        with open(LOG_FILE, 'a') as file:
+            file.write(json.dumps(request.args) + '\n')
+        return Response(status=200)
+    else:
+        return Response(status=404)
